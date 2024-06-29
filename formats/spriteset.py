79618b27190ae4d6d8971829e6da40046613d47f
Eggbertx <eggbertx@users.noreply.github.com>
2024-06-29 13:52:36 -0700

    Fix spriteset direction loading. Having an optional list argument causes it to have the list contents be shared if it is called multiple times (spriteset.py line 19)

--- formats/spriteset.py
+++ formats/spriteset.py
@@ -9,6 +9,12 @@ from PySide6.QtGui import QImage
 class SpritesetFrame:
 	imageIndex:int
 	delay:int
+
+	@staticmethod
+	def fromReader(reader: BufferedReader):
+		(index, delay) = struct.unpack("<HH4x", reader.read(8))
+		return SpritesetFrame(index, delay)
+
 	def __init__(self, index:int, delay:int):
 		self.imageIndex = index
 		self.delay = delay
@@ -16,12 +22,18 @@ class SpritesetFrame:
 class SpritesetDirection:
 	name:str
 	frames:list[SpritesetFrame]
-	def __init__(self, name:str, frames:list[SpritesetFrame] = []):
+
+	@staticmethod
+	def fromReader(reader: BufferedReader):
+		numFrames = struct.unpack("<H6x", reader.read(8))[0]
+		direction = SpritesetDirection(readSphereString(reader), [])
+		for f in range(numFrames):
+			direction.frames.append(SpritesetFrame.fromReader(reader))
+		return direction
+
+	def __init__(self, name:str, frames:list[SpritesetFrame]):
 		self.name = name
 		self.frames = frames
-
-	def addFrame(self, imageIndex:int, delay:int):
-		self.frames.append(SpritesetFrame(imageIndex, delay))
 
 class SphereSpriteset(SphereFile):
 	filePath: str|None
@@ -67,16 +79,11 @@ class SphereSpriteset(SphereFile):
 					numBytes = self.frameWidth * self.frameHeight * 4
 					imgBytes = file.read(numBytes)
 					self.images.append(QImage(imgBytes, self.frameWidth, self.frameHeight, QImage.Format.Format_RGBA8888))
-
 				for d in range(self.numDirection):
-					numFrames = struct.unpack("<H6x", file.read(8))[0]
-					direction = SpritesetDirection(readSphereString(file))
-					for f in range(numFrames):
-						(index, delay) = struct.unpack("<HH4x", file.read(8))
-						direction.addFrame(index, delay)
-					self.directions.append(direction)
+					self.directions.append(SpritesetDirection.fromReader(file))
 			case _:
 				raise FormatException(self.filePath, "spriteset versions 1 and 2 are not supported yet")
+		print("Done parsing")
 
 	def _packBytes() -> bytes:
 		return bytes([])
--- widgets/spriteseteditor.py
+++ widgets/spriteseteditor.py
@@ -1,30 +1,51 @@
-from PySide6.QtCore import Qt
+from PySide6.QtCore import Qt, Slot
 from PySide6.QtGui import QPixmap
 from PySide6.QtWidgets import QWidget, QGraphicsPixmapItem, QGraphicsScene
 
 from formats.spriteset import SphereSpriteset
+from .sphereeditor import SphereEditor, SphereFile
+
 from ui.ui_spriteseteditor import Ui_SpritesetEditor
 
-class SpritesetEditor(QWidget):
+class SpritesetEditor(SphereEditor):
 	ui: Ui_SpritesetEditor
 	loadedSpriteset: SphereSpriteset
+	directionItems: list[QGraphicsPixmapItem]
 	def __init__(self, parent: QWidget = None, windowType: Qt.WindowType = Qt.WindowType.Widget) -> None:
 		super().__init__(parent, windowType)
 		self.ui = Ui_SpritesetEditor()
 		self.ui.setupUi(self)
+		self.editorType = SphereFile.Spriteset
 		self.loadedSpriteset = None
 		self.ui.animView.setScene(QGraphicsScene(self.ui.animView))
+		self.ui.animDirChoose.currentIndexChanged.connect(self.onDirectionChanged)
+		self.directionItems = []
 		self._adjustSizes()
 
 	def _adjustSizes(self):
 		self.ui.ssViewSplitter.setStretchFactor(0,4)
 		self.ui.ssViewSplitter.setSizes((self.window().width()-164,164))
 		self.ui.choosers.setSizes((200,150,200))
-	
+
+	def setDirection(self, d:int):
+		self.ui.animView.scene().clear()
+		self.ui.animView.scene().addItem(self.directionItems[d])
+
 	def attachSpriteset(self, spriteset:SphereSpriteset):
-		item = QGraphicsPixmapItem(QPixmap.fromImage(spriteset.images[0]))
-		self.ui.animView.scene().addItem(item)
 		self.ui.animDirChoose.clear()
-		for dir in spriteset.directions:
-			self.ui.animDirChoose.addItem(dir.name)
+		for direction in spriteset.directions:
+			if len(direction.frames) > 0:
+				imageIndex = direction.frames[0].imageIndex
+				item = QGraphicsPixmapItem(QPixmap.fromImage(spriteset.images[imageIndex]))
+				item.setScale(2)
+				self.directionItems.append(item)
+				self.ui.animDirChoose.addItem(direction.name)
+		self.setDirection(0)
 		self.loadedSpriteset = spriteset
+
+	@Slot(int)
+	def onDirectionChanged(self, d:int):
+		if self.loadedSpriteset is None or d < 0 or d >= len(self.loadedSpriteset.directions):
+			return
+		self.setDirection(d)
+		
