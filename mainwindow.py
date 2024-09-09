afb4f8a3b2b86791f2fe978cb10a2bea2d5c048b
Eggbertx <eggbertx@users.noreply.github.com>
2024-09-09 16:01:29 -0700

    Clean up MainWindow class

--- mainwindow.py
+++ mainwindow.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 from argparse import ArgumentParser
 import ctypes
 from enum import Enum
@@ -29,6 +31,7 @@ from widgets.startpage import StartPage
 from widgets.textedit import TextEdit
 from settings import Settings
 
+
 _VERSION = "0.10"
 _APPLICATION_NAME = "QtSphere IDE"
 _ORG_NAME = "Spherical"
@@ -49,6 +52,7 @@ _OPEN_DIALOG_FILTER = (
 	"All files (*.*)"
 )
 
+
 class FileType(Enum):
 	AllSupported = 0
 	Projects = 1
@@ -61,10 +65,12 @@ class FileType(Enum):
 	WindowStyle = 8
 	All = 9
 
+
 class SidebarTab(Enum):
 	FileTree = 0
 	TaskList = 1
 	SoundTest = 2
+
 
 class MainWindow(QMainWindow):
 	startPage: StartPage
@@ -88,6 +94,7 @@ class MainWindow(QMainWindow):
 	@property
 	def currentTabIndex(self):
 		return self.ui.openFileTabs.currentIndex()
+
 
 	def __init__(self, parent=None, verbose=False):
 		super().__init__(parent)
@@ -147,15 +154,15 @@ class MainWindow(QMainWindow):
 		self.ui.actionExit.triggered.connect(sys.exit)
 		self.ui.actionAbout_Qt.triggered.connect(lambda: QMessageBox.aboutQt(self, "About Qt"))
 		self.ui.actionAbout.triggered.connect(lambda: QMessageBox.about(self, "About QtSphere IDE", _ABOUT_STRING))
-		self.ui.openFileTabs.tabCloseRequested.connect(self.tabCloseRequested)
+		self.ui.openFileTabs.tabCloseRequested.connect(self.onTabCloseRequested)
 		self.ui.openFileTabs.tabBar().tabMoved.connect(self.onTabMoved)
-		self.ui.actionConfigure_QtSphere_IDE.triggered.connect(self.openSettingsWindow)
+		self.ui.actionConfigure_QtSphere_IDE.triggered.connect(self.onOpenSettingsWindowTriggered)
 		self.settingsWindow.settingsSaved.connect(self.onSettingsSaved)
-		self.startPage.projectLoaded.connect(self.loadProject)
-		self.startPage.loadProjectAction.triggered.connect(self.loadSelectedProject)
-		self.startPage.startGameAction.triggered.connect(self.startGame)
-		self.startPage.openProjectDirAction.triggered.connect(self.openSelectedProjectDir)
-		self.engineSelector.currentIndexChanged.connect(self.engineChanged)
+		self.startPage.projectLoaded.connect(lambda proj: self.loadProject(proj))
+		self.startPage.loadProjectAction.triggered.connect(self.onStartPageProjectSelected)
+		self.startPage.startGameAction.triggered.connect(self.onStartGameTriggered)
+		self.startPage.openProjectDirAction.triggered.connect(self.onOpenSelectedProjectDir)
+		self.engineSelector.currentIndexChanged.connect(self.onEngineChanged)
 		self.ui.actionClose.triggered.connect(self.closeProject)
 		self.ui.actionRefresh.triggered.connect(self._updateTree)
 		self.ui.actionProject_Explorer.triggered.connect(lambda: self.switchSidebarTab(SidebarTab.FileTree))
@@ -165,9 +172,9 @@ class MainWindow(QMainWindow):
 		self.ui.actionQSIGithub.triggered.connect(lambda: QDesktopServices.openUrl("https://github.com/Eggbertx/QtSphere-IDE"))
 		self.ui.actionNSGithub.triggered.connect(lambda: QDesktopServices.openUrl("https://github.com/spheredev/neoSphere"))
 		self.ui.actionOpen_Game_Directory.triggered.connect(self._openCurrentProjectDir)
-		self.ui.treeView.activated.connect(self.treeItemActivated)
-		self.ui.actionOpenFile.triggered.connect(self.openFileButtonPressed)
-		self.ui.actionOpenProject.triggered.connect(self.openProjectPressed)
+		self.ui.treeView.activated.connect(self.onTreeItemActivated)
+		self.ui.actionOpenFile.triggered.connect(self.onOpenFileTriggered)
+		self.ui.actionOpenProject.triggered.connect(self.onOpenProjectTriggered)
 		self.ui.openFileTabs.currentChanged.connect(self.onTabChanged)
 		self.ui.actionUndo.triggered.connect(self.onUndoTriggered)
 		self.ui.actionRedo.triggered.connect(self.onRedoTriggered)
@@ -183,7 +190,7 @@ class MainWindow(QMainWindow):
 		self.ui.actionSave.triggered.connect(lambda: self.saveCurrentTab(False))
 		self.ui.actionSave_As.triggered.connect(lambda: self.saveCurrentTab(True))
 		self.ui.newPlainTextFile.triggered.connect(self.newTextFile)
-		self.modifiedFilesDialog.buttonBox.clicked.connect(self.modifiedFileDialogButtonClicked)
+		self.modifiedFilesDialog.buttonBox.clicked.connect(self.onModifiedFileDialogButtonClicked)
 
 		QShortcut(Qt.Modifier.CTRL | Qt.Key.Key_PageDown, self, self.onNextTabTriggered)
 		QShortcut(Qt.Modifier.CTRL | Qt.Key.Key_PageUp, self, self.onPrevTabTriggered)
@@ -193,7 +200,8 @@ class MainWindow(QMainWindow):
 		if self.loadedProject is not None:
 			QDesktopServices.openUrl(self.loadedProject.projectDir)
 
-	def _updateTree(self, game: QSIProject):
+
+	def _updateTree(self, game: QSIProject|None):
 		if game is None:
 			self.ui.treeView.setModel(self.emptyProjectModel)
 			return
@@ -205,6 +213,7 @@ class MainWindow(QMainWindow):
 			if i > 0:
 				self.ui.treeView.hideColumn(i)
 
+
 	def launchGame(self, game:QSIProject):
 		if game is None or game.buildDir is None or game.buildDir == "":
 			QMessageBox.critical(self, "Error launching game", "A game does not appear to be loaded")
@@ -212,15 +221,18 @@ class MainWindow(QMainWindow):
 		
 		self.launcher.launchGame(game)
 
+
 	def newTextFile(self):
 		editor = TextEdit(self.ui.openFileTabs)
 		editor.setModified(True)
 		self.openAndGoToNewEditorWidget(editor, "")
 
+
 	def openAndGoToNewEditorWidget(self, editor:QWidget, filePath:str):
 		self.openFilePaths.append(filePath)
 		t = self.ui.openFileTabs.addTab(editor, "<new file>" if filePath == "" else basename(filePath))
 		self.ui.openFileTabs.setCurrentIndex(t)
+
 
 	def openFile(self, filePath:str):
 		ext = ""
@@ -259,6 +271,7 @@ class MainWindow(QMainWindow):
 			QMessageBox.critical(self, "Error", traceback.format_exc())
 			raise
 
+
 	def closeProject(self):
 		self.loadedProject = None
 		self._updateTree(None)
@@ -267,6 +280,7 @@ class MainWindow(QMainWindow):
 		self.ui.actionProject_Properties.setEnabled(False)
 		self.ui.toolbarPlayGame.setEnabled(False)
 		self.newMapDialog.projectPath = None
+
 
 	def switchSidebarTab(self, tab:SidebarTab):
 		match tab:
@@ -277,15 +291,18 @@ class MainWindow(QMainWindow):
 			case SidebarTab.SoundTest:
 				self.ui.sideBar.tabBar().setCurrentIndex(2)
 
+
 	def showOpenFileDialog(self, fileType: FileType = FileType.AllSupported, startDir:str = ".", title:str = "Open File") -> str|None:
 		result = QFileDialog.getOpenFileName(self, title, startDir,
 			";;".join(_OPEN_DIALOG_FILTER), _OPEN_DIALOG_FILTER[fileType.value])
 		return result[0] if len(result) > 0 and result[0] != "" else None
 
+
 	def showSaveFileDialog(self, fileType: FileType = FileType.AllSupported, startDir:str = ".", title:str = "Save File"):
 		result = QFileDialog.getSaveFileName(self, title, startDir,
 			";;".join(_OPEN_DIALOG_FILTER), _OPEN_DIALOG_FILTER[fileType.value])
 		return result[0] if len(result) > 0 and result[0] != "" else None
+
 
 	def saveCurrentTab(self, saveAs:bool):
 		index = self.currentTabIndex
@@ -306,25 +323,31 @@ class MainWindow(QMainWindow):
 			QMessageBox.critical(self, "Error", traceback.format_exc())
 			raise
 
+
 	def currentTabIsModified(self):
 		active = self.currentTabWidget
 		return hasattr(active, "isModified") and active.isModified()
+
 
 	def setCurrentTabSetModified(self, modified:bool):
 		active = self.currentTabWidget
 		if hasattr(active, "setModified"):
 			active.setModified(modified)
 
+
 	def currentTabCanCutCopyPaste(self):
 		active = self.currentTabWidget
 		return hasattr(active, "cut") and hasattr(active, "copy") and hasattr(active, "paste")
+
 
 	def currentTabHasUndoRedo(self):
 		active = self.currentTabWidget
 		return hasattr(active, "undo") and hasattr(active, "redo")
 
+
 	def currentTabHasSelectAll(self):
 		return hasattr(self.currentTabWidget, "selectAll")
+
 
 	def showSaveConfirmationDialog(self):
 		msgbox = QMessageBox(self)
@@ -338,12 +361,14 @@ class MainWindow(QMainWindow):
 		msgbox.setInformativeText("Do you want to save your changes?")
 		return msgbox.exec()
 
+
 	def setStatus(self, status:str, timeout:int = 0):
 		if status is None or status == "":
 			self.ui.statusBar.clearMessage()
 		else:
 			self.ui.statusBar.showMessage(status, timeout)
-	
+
+
 	def closeEvent(self, event: QCloseEvent):
 		tabs = self.ui.openFileTabs.count()
 		self.modifiedFilesDialog.clearPaths()
@@ -359,6 +384,7 @@ class MainWindow(QMainWindow):
 			event.ignore()
 		else:
 			super().closeEvent(event)
+
 
 	def getExtensionFilterIndex(self, ext:str) -> int:
 		match ext:
@@ -377,6 +403,7 @@ class MainWindow(QMainWindow):
 			case ".rws":
 				return FileType.WindowStyle.value
 		return FileType.All.value
+
 
 	def saveAllModified(self):
 		tabs = self.ui.openFileTabs.count()
@@ -397,10 +424,23 @@ class MainWindow(QMainWindow):
 			editor.save(saveFilename)
 		return True
 
-	#region Slots
 
+	def loadProject(self, project:QSIProject):
+		print("Loading project:", project.projectDir)
+		self.setWindowTitle(f"QtSphereIDE {_VERSION} - {project.name}")
+		self.ui.menuProject.setEnabled(True)
+		self.ui.actionProject_Properties.setEnabled(True)
+		self.loadedProject = project
+		self.ui.toolbarPlayGame.setEnabled(True)
+		self.newMapDialog.projectPath = project.projectDir
+		self.projectPropertiesDialog.project = self.loadedProject
+		self._updateTree(project)
+		self.switchSidebarTab(SidebarTab.FileTree)
+
+
+	#region Slots
 	@Slot(QAbstractButton)
-	def modifiedFileDialogButtonClicked(self, btn:QAbstractButton):
+	def onModifiedFileDialogButtonClicked(self, btn:QAbstractButton):
 		match self.modifiedFilesDialog.buttonBox.standardButton(btn):
 			case QDialogButtonBox.StandardButton.SaveAll:
 				if self.saveAllModified():
@@ -420,9 +460,11 @@ class MainWindow(QMainWindow):
 		self.openAndGoToNewEditorWidget(editor, "")
 		editor.modificationChanged.connect(self.onCurrentFileModificationChanged)
 
+
 	@Slot()
 	def onGameLaunched(self):
 		self.launchGame(self.loadedProject)
+
 
 	@Slot(bool)
 	def onCurrentFileModificationChanged(self, modified:bool):
@@ -454,15 +496,18 @@ class MainWindow(QMainWindow):
 			self.ui.actionSave.setEnabled(currentPath is not None)
 			self.ui.actionSave_As.setEnabled(currentPath is not None)
 
+
 	@Slot()
 	def onNextTabTriggered(self):
 		next = self.currentTabIndex + 1 if self.currentTabIndex+1 < len(self.openFilePaths) else 0
 		self.ui.openFileTabs.setCurrentIndex(next)
 
+
 	@Slot()
 	def onPrevTabTriggered(self):
 		prev = self.currentTabIndex - 1 if self.currentTabIndex > 0 else len(self.openFilePaths) - 1
 		self.ui.openFileTabs.setCurrentIndex(prev)
+
 
 	@Slot()
 	def onFileSaveTriggered(self):
@@ -471,34 +516,41 @@ class MainWindow(QMainWindow):
 			QMessageBox.critical(self, "Error", "Invalid tab path. This should not normally happen.")
 			return
 
+
 	@Slot()
 	def onUndoTriggered(self):
 		if self.currentTabHasUndoRedo():
 			self.currentTabWidget.undo()
 
+
 	def onRedoTriggered(self):
 		if self.currentTabHasUndoRedo():
 			self.currentTabWidget.redo()
+
 
 	@Slot()
 	def onCutTriggered(self):
 		if self.currentTabCanCutCopyPaste():
 			self.ui.openFileTabs.currentWidget().cut()
 
+
 	@Slot()
 	def onCopyTriggered(self):
 		if self.currentTabCanCutCopyPaste():
 			self.ui.openFileTabs.currentWidget().copy()
+
 
 	@Slot()
 	def onPasteTriggered(self):
 		if self.currentTabCanCutCopyPaste():
 			self.ui.openFileTabs.currentWidget().paste()
 
+
 	@Slot()
 	def onSelectAllTriggered(self):
 		if self.currentTabHasSelectAll():
 			self.ui.openFileTabs.currentWidget().selectAll()
+
 
 	@Slot()
 	def onSettingsSaved(self):
@@ -506,15 +558,17 @@ class MainWindow(QMainWindow):
 		self.startPage.refreshGameList()
 		self.engineSelector.setCurrentIndex(1 if settings.whichEngine == "legacy" else 0)
 
+
 	@Slot()
-	def openFileButtonPressed(self):
+	def onOpenFileTriggered(self):
 		filePath = self.showOpenFileDialog(FileType.AllSupported,
 			self.loadedProject.projectDir if self.loadedProject is not None else ".")
 		if filePath is not None:
 			self.openFile(filePath)
 
+
 	@Slot()
-	def openProjectPressed(self):
+	def onOpenProjectTriggered(self):
 		settings = Settings()
 		projectDirs = settings.projectDirs
 		startDir = None if len(projectDirs) == 0 else projectDirs[0]
@@ -527,15 +581,18 @@ class MainWindow(QMainWindow):
 		else:
 			QMessageBox.critical(self, "Error", "Unable to open project directory")
 
+
 	@Slot()
-	def treeItemActivated(self, index: QModelIndex):
+	def onTreeItemActivated(self, index: QModelIndex):
 		if not self.fsModel.isDir(index):
 			self.openFile(self.fsModel.filePath(index))
 
+
 	@Slot()
-	def openSettingsWindow(self):
+	def onOpenSettingsWindowTriggered(self):
 		self.settingsWindow.loadSettings()
 		self.settingsWindow.show()
+
 
 	@Slot(int,int)
 	def onTabMoved(self, fromIndex:int, toIndex:int):
@@ -547,7 +604,7 @@ class MainWindow(QMainWindow):
 
 
 	@Slot(int)
-	def tabCloseRequested(self, index:int):
+	def onTabCloseRequested(self, index:int):
 		if self.currentTabIsModified():
 			match self.showSaveConfirmationDialog():
 				case QMessageBox.StandardButton.Save:
@@ -569,8 +626,9 @@ class MainWindow(QMainWindow):
 			self.openFilePaths.append(None)
 			# self.startPage.refreshGameList()
 
+
 	@Slot(int)
-	def engineChanged(self, index:int):
+	def onEngineChanged(self, index:int):
 		settings = Settings()
 		match index:
 			case 0:
@@ -580,40 +638,31 @@ class MainWindow(QMainWindow):
 				settings.whichEngine ="legacy"
 				self.ui.actionLegacyConfig.setEnabled(True)
 
+
 	@Slot()
-	def startGame(self):
+	def onStartGameTriggered(self):
 		game = self.startPage.selectedGame()
 		if game is None:
 			return
 		self.launchGame(game)
 
+
 	@Slot()
-	def loadSelectedProject(self):
+	def onStartPageProjectSelected(self):
 		selected = self.startPage.selectedGame()
 		if selected is None:
 			self.closeProject()
 		else:
 			self.loadProject(selected)
 
-	@Slot(QSIProject)
-	def loadProject(self, project:QSIProject):
-		print("Loading project:", project.projectDir)
-		self.setWindowTitle(f"QtSphereIDE {_VERSION} - {project.name}")
-		self.ui.menuProject.setEnabled(True)
-		self.ui.actionProject_Properties.setEnabled(True)
-		self.loadedProject = project
-		self.ui.toolbarPlayGame.setEnabled(True)
-		self.newMapDialog.projectPath = project.projectDir
-		self.projectPropertiesDialog.project = self.loadedProject
-		self._updateTree(project)
-		self.switchSidebarTab(SidebarTab.FileTree)
 
 	@Slot()
-	def openSelectedProjectDir(self):
+	def onOpenSelectedProjectDir(self):
 		selected = self.startPage.selectedGame()
 		project = self.startPage.currentProject if selected is None else selected
 		QDesktopServices.openUrl(QUrl.fromLocalFile(project.projectDir))
 	#endregion
+
 
 if __name__ == "__main__":
 	parser = ArgumentParser()
