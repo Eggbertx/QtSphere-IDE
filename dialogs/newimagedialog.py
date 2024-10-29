6197dfd84efb1fe4bd5eff78b6b74b2985e079b8
Eggbertx <eggbertx@users.noreply.github.com>
2024-10-29 11:19:24 -0700

    Add NewImageDialog class and UI

---
+++ dialogs/newimagedialog.py
@@ -0,0 +1,25 @@
+from PySide6.QtCore import Qt
+from PySide6.QtGui import QColor
+from PySide6.QtWidgets import QDialog, QWidget
+
+from ui.ui_newimagedialog import Ui_NewImageDialog
+
+class NewImageDialog(QDialog):
+	ui: Ui_NewImageDialog
+
+	@property
+	def imageWidth(self) -> int:
+		return self.ui.width_num.value()
+
+	@property
+	def imageHeight(self) -> int:
+		return self.ui.height_num.value()
+
+	@property
+	def fillColor(self) -> QColor:
+		return self.ui.fill_btn.color()
+
+	def __init__(self, parent: QWidget | None = None,):
+		super().__init__(parent)
+		self.ui = Ui_NewImageDialog()
+		self.ui.setupUi(self)
