6479c589ee20ed65e6c7de44a8e78d71f95e37a7
Eggbertx <eggbertx@users.noreply.github.com>
2025-10-08 22:42:38 -0700

    Make neoSphere project behave (mostly) the same as 1.x)

--- tests/projects/QSI neoSphere Test Project/Cellscript.js
+++ tests/projects/QSI neoSphere Test Project/Cellscript.js
@@ -10,5 +10,8 @@ describe({
 	main: '@/scripts/main.js',
 });
 
+for(const dir of ["fonts", "maps", "spritesets", "windowstyles"]) {
+	install(dir, files(`../QSI 1.x Test Project/${dir}/*`, true));
+}
 install('scripts', files('scripts/*.js', true));
 install('@/', files('icon.png'));
--- tests/projects/QSI neoSphere Test Project/scripts/main.js
+++ tests/projects/QSI neoSphere Test Project/scripts/main.js
@@ -1,17 +1,45 @@
 import { Task } from "sphere-runtime";
 
+const windowTileBG = LoadWindowStyle("@/windowstyles/window_tile-bg.rws");
+const windowStretchBG = LoadWindowStyle("@/windowstyles/window_stretch-bg.rws");
+const windowGradientBG = LoadWindowStyle("@/windowstyles/window_gradient-bg.rws");
+const windowGradientTiledBG = LoadWindowStyle("@/windowstyles/window_gradient-tiled-bg.rws");
+const windowGradientStretchBG = LoadWindowStyle("@/windowstyles/window_gradient-stretch-bg.rws");
+// const font = LoadFont("@/fonts/dailybuild.rfn");
+const font = new Font("@/fonts/dailybuild.rfn");
+
 export default class Game extends Task {
 	constructor() {
 		super();
 	}
 
 	async on_startUp() {
-		Sphere.abort("test")
+		SetRenderScript(() => this.on_render());
+		SetUpdateScript(() => this.on_update());
+		MapEngine("layerdrawing.rmp", 60);
 	}
 
 	on_update() {
+		if(Keyboard.Default.isPressed(Key.Escape)) {
+			Sphere.shutDown();
+		}
 	}
 
 	on_render() {
+		var x = 32;
+		var y = 32;
+		const w = 120;
+		const h = 64;
+		windowTileBG.drawWindow(x, y, w, h);
+		font.drawText(Surface.Screen, x, y, "#dailybuild");
+		x += w + 64;
+		windowStretchBG.drawWindow(x, y, w*1.4, h);
+		x += w*1.4 + 64;
+		windowGradientBG.drawWindow(x, y, w, h);
+		y += h + 64;
+		x = 32;
+		windowGradientTiledBG.drawWindow(x, y, w, h);
+		x += w + 64;
+		windowGradientStretchBG.drawWindow(x, y, w*1.4, h);
 	}
 }
