74806107ec0dd82004048676f1e0d811ab8a5228
Eggbertx <eggbertx@users.noreply.github.com>
2025-10-08 17:04:57 -0700

    Add font and windowstyle for testing

--- tests/projects/QSI 1.x Test Project/scripts/main.js
+++ tests/projects/QSI 1.x Test Project/scripts/main.js
@@ -1,3 +1,30 @@
+const windowTileBG = LoadWindowStyle("window_tile-bg.rws");
+const windowStretchBG = LoadWindowStyle("window_stretch-bg.rws");
+const windowGradientBG = LoadWindowStyle("window_gradient-bg.rws");
+const windowGradientTiledBG = LoadWindowStyle("window_gradient-tiled-bg.rws");
+const windowGradientStretchBG = LoadWindowStyle("window_gradient-stretch-bg.rws");
+const font = LoadFont("dailybuild.rfn");
+
+
+function render() {
+	var x = 32;
+	var y = 32;
+	const w = 120;
+	const h = 64;
+	windowTileBG.drawWindow(x, y, w, h);
+	font.drawText(x, y, "#dailybuild");
+	x += w + 64;
+	windowStretchBG.drawWindow(x, y, w*1.4, h);
+	x += w*1.4 + 64;
+	windowGradientBG.drawWindow(x, y, w, h);
+	y += h + 64;
+	x = 32;
+	windowGradientTiledBG.drawWindow(x, y, w, h);
+	x += w + 64;
+	windowGradientStretchBG.drawWindow(x, y, w*1.4, h);
+}
+
 function game() {
-	MapEngine("layerdrawing.rmp",60);
+	SetRenderScript("render()");
+	MapEngine("layerdrawing.rmp", 60);
 }
