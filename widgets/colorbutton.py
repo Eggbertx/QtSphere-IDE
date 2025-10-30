a3079eaa715ab0fba16bfacb3b2ae6effd3520ad
Eggbertx <eggbertx@users.noreply.github.com>
2025-10-30 12:17:43 -0700

    Draw checker tile as background on ColorButton for transparency

--- widgets/colorbutton.py
+++ widgets/colorbutton.py
@@ -1,21 +1,22 @@
-from PySide6.QtGui import QColor, QColorConstants, QMouseEvent, QPixmap
-from PySide6.QtCore import QSize, Qt, Signal
+from typing import override
+from PySide6.QtGui import QColor, QColorConstants, QMouseEvent, QPixmap, QPainter
+from PySide6.QtCore import QSize, Qt, Signal, QRect
 from PySide6.QtWidgets import QToolButton, QWidget, QStyle, QColorDialog
+
 
 class ColorButton(QToolButton):
 	color: QColor
 	colorChanged: Signal = Signal(QColor)
 	colorIcon: QPixmap
 	pixmapSize: QSize
-	alphaChannel: bool
+	alphaChannel: bool = False
 
 	def __init__(self, parent: QWidget | None = None, color: QColor = QColorConstants.White) -> None:
 		super().__init__(parent)
 		self.color = color
 		metricSize = self.style().pixelMetric(QStyle.PixelMetric.PM_ButtonIconSize)
-		self.pixmapSize = QSize(metricSize*2, metricSize*2)
+		self.pixmapSize = QSize(metricSize * 2, metricSize * 2)
 		self.updateIcon()
-		self.alphaChannel = False
 
 	def setColor(self, color: QColor):
 		if self.color == color or not color.isValid():
@@ -23,19 +24,32 @@ class ColorButton(QToolButton):
 		self.color = color
 		self.colorChanged.emit(self.color)
 		self.updateIcon()
-	
+
+	@override
 	def mouseReleaseEvent(self, event: QMouseEvent) -> None:
 		if event.button() != Qt.MouseButton.LeftButton:
 			return
-		
-		options = QColorDialog.ColorDialogOption.ShowAlphaChannel if self.alphaChannel else 0
+
+		options = (
+			QColorDialog.ColorDialogOption.ShowAlphaChannel
+			if self.alphaChannel
+			else QColorDialog.ColorDialogOption(0)
+		)
 		newCol = QColorDialog.getColor(self.color, self, options=options)
 		self.setDown(False)
 		self.setColor(newCol)
 		return super().mouseReleaseEvent(event)
-	
+
 	def updateIcon(self):
 		self.colorIcon = QPixmap(self.pixmapSize)
-		self.colorIcon.fill(QColor(self.color))
+		painter = QPainter(self.colorIcon)
+		if self.alphaChannel and self.color.alpha() < 255:
+			alphaPixmap = QPixmap(":/res/transparency-bg.png")
+			for y in range(0, self.pixmapSize.height(), alphaPixmap.height()):
+				for x in range(0, self.pixmapSize.width(), alphaPixmap.width()):
+					painter.drawPixmap(x, y, alphaPixmap)
+		painter.fillRect(QRect(0, 0, self.pixmapSize.width(), self.pixmapSize.height()), self.color)
+		painter.end()
+
 		self.setIconSize(self.pixmapSize)
 		self.setIcon(self.colorIcon)
